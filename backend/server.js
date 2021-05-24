c9fcadc519e7b8747965ea4e3ea38d11d8ef8f7a
akhbarona <akhbarona.118140193@student.itera.ac.id>
2021-05-24 23:04:10 +0700

    version 1.0.0

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,7 @@ const app =  express();
 app.use(express.json())
 app.use(cors());
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -32,6 +33,7 @@ app.use('/api/orders', orderRoute);
 app.use('/api/products', productRoute);
 app.use('/api/uploads', uploadRouter);
 
+const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.use(express.static(path.join(__dirname, "frontend/build")));
 app.get('*', (req, res) => {
